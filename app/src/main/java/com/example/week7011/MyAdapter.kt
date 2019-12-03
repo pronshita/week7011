199fb33f37aabceb77d30ef424bd6a49f9c95882
prinshit <cherry254321@gmail.com>
2019-12-03 15:47:41 +0700

    Initial commit

---
+++ app/src/main/java/com/example/week7011/MyAdapter.kt
@@ -0,0 +1,37 @@
+package com.example.week7011
+
+import android.content.Context
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import kotlinx.android.synthetic.main.model_laout.view.*
+
+class MyAdapter(var dataList : List<DataModel> ,var context: Context)
+    : RecyclerView.Adapter<ViewHoleder>()
+
+{
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHoleder {
+        return ViewHoleder(LayoutInflater.from(context).inflate(R.layout.model_laout,parent,false))
+    }
+
+    override fun getItemCount(): Int {
+        return dataList.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHoleder, position: Int) {
+        try {
+            holder?.textView.text = dataList.get(position).title
+            Glide.with(context).load(dataList.get(position).url).into(holder?.imageView)
+        }catch (errorVar:Exception){
+            Log.e("Error",errorVar.message)
+        }
+    }
+}
+
+class ViewHoleder(view: View):RecyclerView.ViewHolder(view){
+    var imageView = view.img_photo
+    var textView = view.tv_title
+}
